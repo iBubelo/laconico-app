bb0e4949ccdbddc9650ddb3d629d15e658c82fd4
iBubelo <Ilya@MacBook-Air.local>
2017-09-16 18:32:55 +0200

    improve authorization step 3 via splitting old monolith function into several separate functions

--- public/js/authEnd.js
+++ public/js/authEnd.js
@@ -1,20 +1,34 @@
 // Authorization Step 3
 // On page load convert a request token into a Pocket access token after callback from Pocket
-fetch(pocketConfig.url.authorize, {
-  method: pocketConfig.method,
-  headers: pocketConfig.headers,
-  body:
-    "consumer_key=" +
-    consumerKey +
-    "&code=" +
-    localStorage.getItem("request_token")
-})
-  .then(response => response.json())
-  // Extract the access_token and save into localStorage
-  .then(access_token => {
-    localStorage.setItem("access_token", access_token.access_token);
-    window.location.href = "http://127.0.0.1:3000/";
+
+// Request to convert request token and return JSON
+function convertRequestToken() {
+  return fetch(pocketConfig.url.authorize, {
+    method: pocketConfig.method,
+    headers: pocketConfig.headers,
+    body:
+      "consumer_key=" +
+      consumerKey +
+      "&code=" +
+      localStorage.getItem("request_token")
   })
-  .catch(err => {
-    console.log("Error during token obtaining: ", err);
-  });
+  .then(response => response.json());
+}
+
+// Save access_token to localStorage and return to main page
+function saveAccessToken(accessToken) {
+  localStorage.setItem("access_token", accessToken.access_token);
+}
+
+function returnToMainPage() {
+  window.location.href = "http://127.0.0.1:3000/";
+}
+
+function endAuthorization() {
+  convertRequestToken()
+    .then(saveAccessToken)
+    .then(returnToMainPage)
+    .catch(err => {
+      console.log("Error during token obtaining: ", err);
+    });
+}
